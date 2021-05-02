501f7d7a3d9bea8188ceabb386d77a4f5bab8145
Kayinnasaki <kayinnasaki@gmail.com>
2021-05-02 03:53:25 -0400

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,103 @@
+var appConfig = {
+    controlws: "ws://localhost:8088",
+    listenws: "ws://localhost:8089"
+};
+
+var appPlayers = [{
+  label: "Miko",
+  Key: 0,
+  char: "millia",
+  test: "test"
+},
+{
+  label: "Shadow Miko",
+  Key: 1,
+  char: "Dizzy",
+  test: "test"
+},
+{
+  label: "Will",
+  Key: 2,
+  char: "sol",
+  test: "test"
+},
+]
+
+var appTeams = [{
+    label: "Scary Monsters", key: "0",
+    top: "YuzuYEEHAW",
+    topchar: "dizzy",
+    bottom: "Syd",
+    bottomchar: "pot"
+  },
+  {
+    label: "The school of the Undefeated of the South! The winds of the king! Zenshin! Keiretsu! Tenpa Kyoran! Look! the South is burning red", key: "1",
+    top: "Master Chunks",
+    topchar: "ky",
+    bottom: "Knux Kasshu",
+    bottomchar: "may"
+  },
+  {
+    label: "We're Devilman Too", key: "2",
+    top: "Winter",
+    topchar: "ino",
+    bottom: "Satchamobob",
+    bottomchar: "sol"
+  },
+  {
+    label: "Leggy", key: "3",
+    top: "Akai",
+    topchar: "jam",
+    bottom: "Bashe",
+    bottomchar: "zato"
+  },
+  {
+    label: "Demon Girl Gang", key: "4",
+    top: "Ruby",
+    topchar: "ram",
+    bottom: "Shay",
+    bottomchar: "baiken"
+  },
+  {
+    label: "The Chain Gang", key: "5",
+    top: "Imp Retro",
+    topchar: "axl",
+    bottom: "Korbp",
+    bottomchar: "baiken"
+  },
+  {
+    label: "Please Buy YOLO Stock", key: "6",
+    top: "CaliScrub",
+    topchar: "venom",
+    bottom: "CaliBro",
+    bottomchar: "leo"
+  },
+  {
+    label: "The Thigh Inquisitors", key: "7",
+    top: "Blank",
+    topchar: "jam",
+    bottom: "Taiku",
+    bottomchar: "millia"
+  },
+  {
+    label: "「FIGHTING☆DREAMERS」", key: "8",
+    top: "Shawn",
+    topchar: "chipp",
+    bottom: "Code Monkey",
+    bottomchar: "answer"
+  },
+  {
+    label: "Ghoul N Ghosts", key: "9",
+    top: "Bwarch",
+    topchar: "jacko",
+    bottom: "Trynant",
+    bottomchar: "may"
+  },
+  {
+    label: "Head and Shoulders", key: "10",
+    top: "Miko",
+    topchar: "millia",
+    bottom: "Shadow Miko",
+    bottomchar: "dizzy"
+  },
+];
