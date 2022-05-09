19a39297306ddb13d9c2a3b72b5ebef7730b6b15
Kayin <kayinnasaki@gmail.com>
2022-05-09 04:48:21 -0400

    Delete config.js

--- server/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    privateKey: "",
-    certificate: "",
-    apikey: 'ZDEDhI6OtvXrQWFzPgvP1YCnIDPM4b4nD1cmJrGF',
-    controlport:8088,
-    listenport: 8089
-};
-
-module.exports = config;
